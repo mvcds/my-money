1f4aa7054fb87c8f36729d03a64464f598599dcd
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2020-01-04 09:53:58 +0100

    move methods to objects

--- src/3-web/src/Storage/index.js
+++ src/3-web/src/Storage/index.js
@@ -8,60 +8,64 @@ const app = {
   }
 }
 
-const storage = new Storage({
-  entry: {
-    async create (data) {
-      const entry = { ...data, id: app.nextEntryId() }
+const entry = {
+  async create (data) {
+    const entry = { ...data, id: app.nextEntryId() }
 
-      const list = await this.readAll()
+    const list = await this.readAll()
 
-      if (list[entry.id]) return this.create(data)
+    if (list[entry.id]) return this.create(data)
 
-      list[entry.id] = entry
+    list[entry.id] = entry
 
-      localStorage.setItem('my-money/entries', JSON.stringify(list))
+    localStorage.setItem('my-money/entries', JSON.stringify(list))
 
-      return entry
-    },
-    async readAll () {
-      const all = localStorage.getItem('my-money/entries') || '{}'
-
-      return JSON.parse(all)
-    }
+    return entry
   },
-  projection: {
-    async read ({ id }) {
-      const all = await this.readAll()
+  async readAll () {
+    const all = localStorage.getItem('my-money/entries') || '{}'
 
-      return all[id]
-    },
-    async update (data) {
-      const all = await this.readAll()
+    return JSON.parse(all)
+  }
+}
 
-      if (!all[data.id]) return
+const projection = {
+  async read ({ id }) {
+    const all = await this.readAll()
 
-      localStorage.setItem('my-money/projections', JSON.stringify(all))
-    },
-    async readAll () {
-      const all = localStorage.getItem('my-money/projections')
+    return all[id]
+  },
+  async update (data) {
+    const all = await this.readAll()
 
-      if (all === null) {
-        const firstRun = {
-          default: {
-            id: 'default',
-            title: 'Standard',
-            entries: []
-          }
+    if (!all[data.id]) return
+
+    localStorage.setItem('my-money/projections', JSON.stringify(all))
+  },
+  async readAll () {
+    const all = localStorage.getItem('my-money/projections')
+
+    if (all === null) {
+      const firstRun = {
+        default: {
+          id: 'default',
+          title: 'Standard',
+          entries: []
         }
-
-        localStorage.setItem('my-money/projections', JSON.stringify(firstRun))
-
-        return firstRun
       }
 
-      return JSON.parse(all)
+      localStorage.setItem('my-money/projections', JSON.stringify(firstRun))
+
+      return firstRun
     }
+
+    return JSON.parse(all)
   }
+}
+
+const storage = new Storage({
+  entry,
+  projection
 })
 
 export default storage
