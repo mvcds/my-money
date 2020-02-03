53a5e6a38fdfc571a9a959ab0dd714977730b4b0
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2020-02-03 21:39:14 +0100

    create a visual for disabled entries

--- src/3-web/src/App/FinancialScenario/FixedValues/Entry/entry.visual.js
+++ src/3-web/src/App/FinancialScenario/FixedValues/Entry/entry.visual.js
@@ -4,8 +4,14 @@ import Money from '../money'
 import Percentage from '../percentage'
 
 function Entry ({ id, source, value, isDisabled, share, onToggleDisabled }) {
+  const classes = ['financial__entry']
+
+  if (isDisabled) {
+    classes.push('financial__entry--disabled')
+  }
+
   return (
-    <tr>
+    <tr className={classes.join(' ')}>
       <td>{source}</td>
       <Money value={Math.abs(value)} />
       <Percentage value={share} />
